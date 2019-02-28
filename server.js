2a53a0fa92bcfa756a889b89e9beeb99f40f521d
Jordan Epps <jordanepps@gmail.com>
2019-02-28 16:50:28 -0500

    Add middleware for authorization and function for query filters

--- server.js
+++ server.js
@@ -1,13 +1,66 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-	res.send('Hello, world!');
+const validTypes = [
+	`Bug`,
+	`Dark`,
+	`Dragon`,
+	`Electric`,
+	`Fairy`,
+	`Fighting`,
+	`Fire`,
+	`Flying`,
+	`Ghost`,
+	`Grass`,
+	`Ground`,
+	`Ice`,
+	`Normal`,
+	`Poison`,
+	`Psychic`,
+	`Rock`,
+	`Steel`,
+	`Water`
+];
+
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken)
+		return res.status(401).json({ error: 'Unauthorized request' });
+
+	next();
 });
+
+function handleGetTypes(req, res) {
+	res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+	let response = POKEDEX.pokemon;
+
+	if (req.query.name)
+		response = response.filter(pokemon =>
+			pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+		);
+
+	if (req.query.type)
+		response = response.filter(pokemon =>
+			pokemon.type.includes(req.query.type)
+		);
+
+	res.json(response);
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
